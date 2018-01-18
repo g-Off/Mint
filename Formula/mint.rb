e634a1dc2e305b41113620f0d2634e64c0e425b3
Geoffrey Foster <geoff@g-Off.net>
2018-01-18 16:33:07 -0500

    bumping to my own custom version

--- Formula/mint.rb
+++ Formula/mint.rb
@@ -1,9 +1,9 @@
 class Mint < Formula
   desc "Dependency manager that installs and runs Swift command line tool packages"
-  homepage "https://github.com/yonaskolb/Mint"
-  url "https://github.com/yonaskolb/Mint/archive/0.7.1.tar.gz"
-  sha256 "986dd967b818ee044ed0568135db8be2083ad6c3995fe6281ada9b42b224a2ec"
-  head "https://github.com/yonaskolb/Mint.git"
+  homepage "https://github.com/g-Off/Mint"
+  url "https://github.com/g-Off/Mint/archive/0.8.0.tar.gz"
+  sha256 "3b32522f7f65a4e0fe9e186bc9f9d1ad455333eb610f837d59d97aa3f7cb0801"
+  head "https://github.com/g-Off/Mint.git"
 
   depends_on :xcode
 
