3df47fafe167c00ddf4e519b4226035d416fb934
Jared Hendry <hendry.jared@gmail.com>
2019-03-27 15:35:22 -0400

    Update brew to 0.8.1

--- Formula/mint.rb
+++ Formula/mint.rb
@@ -1,9 +1,9 @@
 class Mint < Formula
   desc "Dependency manager that installs and runs Swift command line tool packages"
-  homepage "https://github.com/g-Off/Mint"
-  url "https://github.com/g-Off/Mint/archive/0.8.0.tar.gz"
-  sha256 "3b32522f7f65a4e0fe9e186bc9f9d1ad455333eb610f837d59d97aa3f7cb0801"
-  head "https://github.com/g-Off/Mint.git"
+  homepage "https://github.com/jaredh/Mint"
+  url "https://github.com/jaredh/Mint/archive/0.8.1.tar.gz"
+  sha256 "395408a3dc9c3db2b5c200b8722a13a60898c861633b99e6e250186adffd1370"
+  head "https://github.com/jaredh/Mint.git"
 
   depends_on :xcode
 
